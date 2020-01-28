da21f6dbee9fdab401937d48f26318f18dd1a94b
Yao Jean-Claude <jeandev84@yahoo.com>
2020-01-28 16:55:53 +0300

    Doctrine & Database interaction

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
@@ -50,16 +51,53 @@ class HomeController extends AbstractController
                      ->add('fullname', TextType::class)
                      ->getForm();
 
-
         $person = [
            'name' => 'Jean-Claude',
            'lastname' => 'Yao',
            'age' => 35
         ];
 
-       return $this->render('home/greet.html.twig', [
+        /* ------------------------------------------
+         * Store Stuff in the database
+        ---------------------------------------------*/
+        $post = new Post();
+
+        $post->setTitle("overseas media");
+        $post->setDescription("youtube channel for web shit");
+
+
+        $em = $this->getDoctrine()->getManager();
+
+        /*
+        # return one result where condition
+        $retrievedPost = $em->getRepository(Post::class)->findOneBy(['description' => 'description value');
+
+        # return all result where condition
+        $retrievedPost = $em->getRepository(Post::class)->findBy([
+            'id' => 1
+        ]);
+
+        # return all posts
+        $retrievedPost = $em->getRepository(Post::class)->findAll();
+        */
+        $retrievedPost = $em->getRepository(Post::class)->findOneBy([
+            'id' => 1
+        ]);
+
+        echo '<pre>';
+        var_dump($retrievedPost);
+        echo '</pre>';
+        // create the sql
+        //$em->persist($post);
+        $em->remove($retrievedPost);
+
+        // call it at the end
+        $em->flush();
+
+        return $this->render('home/greet.html.twig', [
           'person' => $person,
-           'user_form'  => $form->createView()
+          'post' => $post,
+          'user_form'  => $form->createView()
        ]);
     }
 }
